84d9248d48c16cf844f841f35f28afe5b2f17047
henzoparahua <hnz.2894henzop19@gmail.com>
2024-08-24 20:16:50 -0300

    basic structure

--- exercicios-js/fundamentos/var.js
+++ exercicios-js/fundamentos/var.js
@@ -1,1 +1,20 @@
 var a = 3;
+var a = 30;
+a = 7;
+
+let b = 4;
+b = 40;
+
+// let b = 30;
+// Uncaught SyntaxError: Identifier 'b' has already been declare
+
+const c = 3;
+const n = null;
+
+// c = 60; var.js:8 
+// Uncaught TypeError: Assignment to constant variable.
+
+// const n;
+// Uncaught SyntaxError: Missing initializer in const declaration 
+
+console.log(c, a, b, n)
